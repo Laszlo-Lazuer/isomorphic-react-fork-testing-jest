161529e5d2e1efd3fd89ef221b509505c5588db5
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-08 12:24:36 -0500

    Jest - teardown globals example.

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,24 @@
 describe("The question list", ()=>{
+    beforeEach(()=>{
+        console.log("Before each!");
+    });
+    beforeAll(()=>{
+        console.log("Before all!");
+    });
+
     it ("should display a list of items", ()=>{
       expect(40 + 2).toEqual(42);
-    })
+    });
+
+    it ("should be the meaning of life", ()=>{
+        expect(40 + 2).toEqual(42);
+    });
+
+    afterEach(()=>{
+        console.log("After each!");
+    });
+
+    afterAll(()=>{
+        console.log("After all!");
+    });
 });
