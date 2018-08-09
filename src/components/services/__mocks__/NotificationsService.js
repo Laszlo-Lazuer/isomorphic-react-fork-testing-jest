81eff95be44931311aa2f758830622482233fe6e
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-09 14:13:14 -0500

    jest - mocks example.

---
+++ src/components/services/__mocks__/NotificationsService.js
@@ -0,0 +1,12 @@
+let count = 0;
+export default {
+    __setCount(_count) {
+        count = _count;
+    },
+    async GetNotifications(){
+        console.warn("GOOD JOB! USING MOCK SERVICE!");
+        return { count };
+    }
+}
+
+//LOCAL MODULES DO NEED TO BE EXPLICITLY CALLED
